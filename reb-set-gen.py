03c44ad66650ff6cce1f545ce5e74755179f6f56
domsson <julien.d.dau@gmail.com>
2022-01-10 01:53:29 +0900

    Replaced image input file requirement with simple sane-name/asciify implementation

--- reb-set-gen.py
+++ reb-set-gen.py
@@ -2,28 +2,27 @@
 
 # concept:
 #
-# read three files,
+# read two files,
 # - format
-# - images
 # - brands
 #
 # `format` contains a one-line format string with placeholders, $IMAGE and $BRAND (or somesuch)
-# `images` is a newline-separated list of image file names
 # `brands` is a newline-separated list of brand names
-#
-# The numer of liens in `images` and `brands` needs to match, and the order of 
-# entries in both files is assumed to match each other
-#
-# We read all three files, then generate a new file that contains one line for 
-# each line of the `images`/`brands` files, where the line is based on the 
-# `format` string, but with the placeholders replaced with the respective 
-# entries from the `images` and `brands` files.
 
 import os
 import sys
 import argparse
 
+#
+# configuration
+#
+
 cfg_anykey_before_abort = (os.name == 'nt')
+cfg_image_extension = "png"
+
+#
+# helper methods
+#
 
 def abort(message, code=0, anykey=False):
     print(message)
@@ -32,31 +31,80 @@ def abort(message, code=0, anykey=False):
     sys.exit(code)
 
 #
+# asciify
+#
+
+asciify_char_map = {
+    "__": "_", # for second iteration, fix adjacent underscores
+    " ": "_", # different to the other space, no idea, keep it!
+    " ": "_", # different to the first space, keep this as well!
+    ",": "_",
+    ";": "_",
+    "&": "",
+    "+": "",
+	"á": "a",
+	"à": "a",
+	"â": "a",
+	"ǎ": "a",
+	"ă": "a",
+	"ä": "ae",
+	"ã": "a",
+	"é": "e",
+	"è": "e",
+	"ê": "e",
+	"ě": "e",
+	"ĕ": "e",
+	"ë": "e",
+	"í": "i",
+	"ì": "i",
+	"î": "i",
+	"ǐ": "i",
+	"ĭ": "i",
+	"ï": "i",
+	"ó": "o",
+	"ò": "o",
+	"ô": "o",
+	"ǒ": "o",
+	"ö": "oe",
+	"õ": "o",
+	"ø": "o",
+	"ɵ": "o",
+	"ú": "u",
+	"ù": "u",
+	"û": "u",
+	"ǔ": "u",
+	"ŭ": "u",
+	"ü": "ue",
+	"ñ": "n",
+	"ý": "y",
+	"ÿ": "y",
+    "ẞ": "ss",
+    "ß": "ss",
+}
+
+def make_lower_ascii(string):
+        string = string.lower()
+        for search, replace in asciify_char_map.items():
+                string = string.replace(search, replace)
+        return string
+
+#
 # argparse
 #
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--format",  help = "Format file", default = "", required=True)
-parser.add_argument("-i", "--images",  help = "Images file", default = "", required=True)
 parser.add_argument("-b", "--brands",  help = "Brands file", default = "", required=True)
 parser.add_argument("-o", "--output",  help = "Output file", default = "", required=True)
 
 args = parser.parse_args()
 
-#format_file = os.path.abspath(args.format)
-#images_file = os.path.abspath(args.images)
-#brands_file = os.path.abspath(args.brands)
-
 format_file = args.format
-images_file = args.images
 brands_file = args.brands
 output_file = args.output
 
 if not os.path.isfile(format_file):
     abort("Not a file: " + format_file, 0, cfg_anykey_before_abort)
-
-if not os.path.isfile(images_file):
-    abort("Not a file: " + images_file, 0, cfg_anykey_before_abort)
 
 if not os.path.isfile(brands_file):
     abort("Not a file: " + brands_file, 0, cfg_anykey_before_abort)
@@ -72,30 +120,23 @@ if format_string.find("$IMAGE") == -1:
 if format_string.find("$BRAND") == -1:
     abort("Format string is missing '$BRAND' placeholder", 0, cfg_anykey_before_abort)
 
-images_handle = open(images_file, 'r') 
-brands_handle = open(brands_file, 'r') 
-
-images_lines = images_handle.readlines() 
-brands_lines = brands_handle.readlines()
-
-num_images_lines = len(images_lines)
+brands_handle    = open(brands_file, 'r') 
+brands_lines     = brands_handle.readlines()
 num_brands_lines = len(brands_lines)
-
-if num_images_lines != num_brands_lines:
-    images_handle.close()
-    brands_handle.close()
-    abort("File line number mismatch: images " + str(num_images_lines) + ", brands " + str(num_brands_lines), 0, cfg_anykey_before_abort)
 
 output_handle = open(output_file, 'w')
 num_output_lines = 0
 
-for x in range(0, num_images_lines):
-    output_string = format_string.replace("$IMAGE", images_lines[x].strip())
-    output_string = output_string.replace("$BRAND", brands_lines[x].strip())
+for x in range(0, num_brands_lines):
+    brand_name = brands_lines[x].strip()
+    image_name = make_lower_ascii(brand_name)
+    image_name = make_lower_ascii(image_name) # second iteration to get rid of double underscores etc
+    image_file = image_name + "." + cfg_image_extension
+    output_string = format_string.replace("$IMAGE", image_file)
+    output_string = output_string.replace("$BRAND", brand_name)
     output_handle.write(output_string + "\n")
     num_output_lines += 1
 
-images_handle.close()
 brands_handle.close()
 output_handle.close()
 
